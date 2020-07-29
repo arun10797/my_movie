fe63e8b500e0c03ffec69dbd0f264f3231ca6aa7
Arun Tiwari <arunt@highfurtados.com>
2020-07-30 02:25:09 +0530

    added db file

--- ratings/models.py
+++ ratings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MovieDetails(models.Model):
-	created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+	created_by = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
 	popularity = models.DecimalField(max_digits=19, decimal_places=10, blank=True)
 	director = models.CharField(max_length=200,blank=True)
 	imdb_score = models.DecimalField(max_digits=19, decimal_places=10, blank=True)
